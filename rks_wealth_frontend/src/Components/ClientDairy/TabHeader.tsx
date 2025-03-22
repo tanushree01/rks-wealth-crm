6ae82778f81a9e6736239595ba9bc61e2a0bb98a
tanushree <tanuagra01@outlook.com>
2025-03-22 23:45:38 +0530

    added tab heder

---
+++ rks_wealth_frontend/src/Components/ClientDairy/TabHeader.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import { useState } from "react";
+import ClientDairy from "./ClientDairy";
+
+
+function TableHeader() {
+  const [selectedTab, setSelectedTab] = useState("clientDiary");
+
+  const handleTabChange = (tab) => {
+    setSelectedTab(tab);
+  };
+
+  return (
+    <>
+      <div className="row">
+        <div className="profile-head">
+          <ul className="nav nav-tabs poppins-black-italic" id="myTab" role="tablist">
+            <li className="nav-item">
+              <button
+                className={`nav-link ${selectedTab === "clientDiary" ? "active" : ""}`}
+                onClick={() => handleTabChange("clientDiary")}
+              >
+                Client Diary
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${selectedTab === "folioMaster" ? "active" : ""}`}
+                onClick={() => handleTabChange("folioMaster")}
+              >
+                Folio Master
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${selectedTab === "longTerm" ? "active" : ""}`}
+                onClick={() => handleTabChange("longTerm")}
+              >
+                Long Term
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${selectedTab === "topSchemeSubscription" ? "active" : ""}`}
+                onClick={() => handleTabChange("topSchemeSubscription")}
+              >
+                Top Scheme Subscription
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${selectedTab === "transaction" ? "active" : ""}`}
+                onClick={() => handleTabChange("transaction")}
+              >
+                Transaction
+              </button>
+            </li>
+          </ul>
+          <div className="container-fluid">
+            <div id="myTabContent">
+              {selectedTab === "folioMaster" && <ClientDairy/>}
+              {/* {selectedTab === "longTerm" && <PendingLeaveReq />}
+              {selectedTab === "topSchemeSubscription" && <LeaveHistory />}
+              {selectedTab === "transaction" && <MonthlyReport />}
+              {selectedTab === "clientDiary" && <MyTeam />} */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TableHeader;
