17e0b1b940adcff6126ee501f023b1f4d6fab514
tanushree <tanuagra01@outlook.com>
2025-03-23 02:12:06 +0530

    fixed the sidebar

--- rks_wealth_frontend/src/Components/ClientDairy/TabHeader.tsx
+++ rks_wealth_frontend/src/Components/ClientDairy/TabHeader.tsx
@@ -27,17 +27,9 @@ const TableHeader = () => {
   }, [selectedTab]);
 
   return (
-    <div className="p-6 bg-gray-100 min-h-screen">
+   
       <div className="mt-1 bg-white shadow-md rounded-lg p-4">
         <div className="flex space-x-4 border-b pb-2">
-          <button
-            className={`px-4 py-2 rounded-t-lg font-semibold ${
-              selectedTab === "clientDiary" ? "bg-[#2c476a] text-white" : "bg-gray-200 text-gray-800"
-            }`}
-            onClick={() => handleTabChange("clientDiary")}
-          >
-            Client Diary
-          </button>
           <button
             className={`px-4 py-2 rounded-t-lg font-semibold ${
               selectedTab === "folioMaster" ? "bg-[#2c476a] text-white" : "bg-gray-200 text-gray-800"
@@ -46,10 +38,25 @@ const TableHeader = () => {
           >
             Folio Master
           </button>
+          <button
+            className={`px-4 py-2 rounded-t-lg font-semibold ${
+              selectedTab === "longTerm" ? "bg-[#2c476a] text-white" : "bg-gray-200 text-gray-800"
+            }`}
+            onClick={() => handleTabChange("longTerm")}
+          >
+           Long Term
+          </button>
+          <button
+            className={`px-4 py-2 rounded-t-lg font-semibold ${
+              selectedTab === "transaction" ? "bg-[#2c476a] text-white" : "bg-gray-200 text-gray-800"
+            }`}
+            onClick={() => handleTabChange("transaction")}
+          >
+            Transactions
+          </button>
         </div>
         <div className="p-4">{renderComponent}</div>
       </div>
-    </div>
   );
 };
 
