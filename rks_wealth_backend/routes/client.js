8df19a55035790d3d8ac7e424828abd0a8e89358
anan230793 <ananditaagrawal@gmail.com>
2025-03-28 21:42:31 +0530

    fixed folio master table

--- rks_wealth_backend/routes/client.js
+++ rks_wealth_backend/routes/client.js
@@ -39,7 +39,7 @@ router.get("/diary", authMiddleware, clientController.getClientDiaries);
 
 
 router.get(
-  "/foliomaste/download",
+  "/foliomaster/download",
   authMiddleware,
   roleMiddleware(["Admin", "RM"]),
   downloadFolioMasterRecords
